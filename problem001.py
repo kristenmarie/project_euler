32f3e755f7d6b7659f216d28a4a3de04dc5a516c
Kristen Kulha <kristen.m.kulha@gmail.com>
2018-04-18 11:58:02 -0700

    Add 2 different solutions for problem 1.

--- problem001.py
+++ problem001.py
@@ -2,10 +2,12 @@
 
 Find the sum of all the multiples of 3 or 5 below 1000."""
 
-def sum():
-  sum = 0
-  for i in range(1,1000):
-    if i % 3 == 0 and i % 5 == 0
-    sum += i
+# def multiples_sum():
+#   total = 0
+#   for i in range(1,1000):
+#     if i % 3 == 0 or i % 5 == 0:
+#       total += i
+#   return total
 
-  return sum
+def multiples_sum():
+  return sum(i for i in range(1000) if (i % 3 == 0 or i % 5 == 0))
