6fdcf4fbe95ae0f03b4336aa0351f864876d649f
Kristen Kulha <kristen.m.kulha@gmail.com>
2018-04-18 13:00:56 -0700

    Add solution for problem 3.

--- problem003.py
+++ problem003.py
@@ -2,3 +2,13 @@
 
 What is the largest prime factor of the number 600851475143 ?"""
 
+def largest_prime():
+  factor = 2
+  num = 600851475143
+  while factor ** 2 <= num:
+    if num % factor == 0:
+      num /= factor
+    factor += 1
+  if (num > 1):
+    return num
+  return factor
